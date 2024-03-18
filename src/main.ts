3052acf1c7648b812c7fa4c58185561f3e21df97
SolarisQuests <160103412+SolarisQuests@users.noreply.github.com>
2024-03-18 18:31:12 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,50 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
+import helmet from "helmet";
+import rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors()
+
+  app.use(helmet.frameguard());
+  app.use(helmet.hidePoweredBy());
+  app.use(helmet.ieNoOpen());
+  app.use(helmet.noSniff());
+  app.use(helmet.xssFilter());
+  app.use(helmet.contentSecurityPolicy());
+  app.use(helmet.crossOriginEmbedderPolicy());
+  app.use(helmet.crossOriginOpenerPolicy());
+  app.use(helmet.crossOriginResourcePolicy());
+  app.use(
+    helmet.dnsPrefetchControl({
+      allow: true,
+    })
+  );
+
+  app.use(helmet.originAgentCluster());
+  app.use(helmet.permittedCrossDomainPolicies());
+  app.use(helmet.referrerPolicy());
+  // const limiter = rateLimit({
+  //   windowMs: 1 * 60 * 1000,
+  //   max: 150,
+  //   standardHeaders: true,
+  //   legacyHeaders: false,
+  // })
+  // app.use(limiter)
+
+  app.use(bodyParser.json({ limit: '100mb' }));
+  app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
+  const option = new DocumentBuilder()
+    .setTitle('ONBOARDING FORM')
+    .setDescription('api')
+    .setVersion('1.0.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, option);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3007);
 }
 bootstrap();
